5124ff08b16bd4c4f3dcf1d639fe917b87e55282
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-19 22:22:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,6 +30,8 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
+  
+  "SELECT COUNT(temperature) FROM bears WHERE temperature = ' goofy';"
 end
 
 def selects_bear_that_killed_Tim
