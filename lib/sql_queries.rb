8517ae4ffcf47abf2beaa94bde0eb9e27433ed45
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-19 22:11:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
-  "SELECT MAX(age), name,age FROM bears;"
+  "SELECT MAX(age), name FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
